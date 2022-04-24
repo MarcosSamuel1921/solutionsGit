486d100be3fa7f8c3bf3bbb9862650d9deb11488
Marcos Samuel <104008807+MarcosSamuel1921@users.noreply.github.com>
2022-04-23 22:04:25 -0300

    Delete problems directory

--- problems/problem-1/1013.py
+++
@@ -1,6 +0,0 @@
-import math
-
-a,b,c = map(int, input().split(" ")) #map para converter em cada valor da lista em um inteiro e o slip para quebrar a string
-
-maior = (a+b+abs(a-b))/2
-maiorABC = (maior + c+abs(maior-c))/2
