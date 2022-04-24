bb8b916e8cb7e0f1cdcfbef421e6f5d8b972db6c
Marcos Samuel <104008807+MarcosSamuel1921@users.noreply.github.com>
2022-04-24 14:24:37 -0300

    Update 3343.py

--- problem-3/3343.py
+++ problem-3/3343.py
@@ -1,1 +1,24 @@
 
+n, x = map(int, input().split())
+
+str = input()
+
+p, m, g= map(int, input().split())
+
+w = y = z = 0
+
+for i in range(len(str)):
+    if str[i] == 'P':
+        w += p
+    if str[i] == 'M':
+        y += m
+    if str[i] == 'G':
+        z += g
+
+totalTitans = w + y +z
+minimoMura = 0
+
+while totalTitans > x or totalTitans > 0:
+    minimoMura += 1
+    totalTitans -= x
+print(minimoMura)
